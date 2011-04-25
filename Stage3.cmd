088afe9419f9dfc68436e94621f69184f56613dc
Johncarlo Spilotro <johncarlo.spilotro@gmail.com>
2011-04-25 19:50:59 -0400

    PCR Tools v1.0 Initial Commit

---
+++ Stage3.cmd
@@ -0,0 +1,181 @@
+:: Off Process PCR Tools Last stage
+
+CLS
+@echo off
+TITLE Stage 3 - Off Process PCR Tool
+
+
+:: CHECK WINDOWS VERSION
+for /f "tokens=2 delims=[]" %%x in ('ver') do set WINVER=%%x 
+set WINVER=%WINVER:Version =% 
+
+ECHO %WINVER%
+
+
+ECHO Detecting OS processor type...
+
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+@echo off
+ECHO 64BIT SYSTEM DETECTED.
+
+set ccleanerpath="C:\Program Files (x86)\CCleaner"
+set mbampath="C:\Program Files (x86)\Malwarebytes' Anti-Malware"
+set saspath="C:\Program Files\SUPERAntiSpyware"
+
+) ELSE (
+@echo off
+ECHO 32BIT SYSTEM DETECTED.
+
+set ccleanerpath="C:\Program Files\CCleaner"
+set mbampath="C:\Program Files\Malwarebytes' Anti-Malware"
+set saspath="C:\Program Files\SUPERAntiSpyware"
+)
+
+ECHO PATHS SET AS..
+ECHO %ccleanerpath%
+ECHO %mbampath%
+ECHO %saspath%
+PAUSE
+
+IF %WINVER% == 5.1.2600 (
+GOTO :WINDOWSXP
+
+) ELSE IF %WINVER% == 5.2.3790 (
+GOTO :WINDOWSXP64
+
+) ELSE IF %WINVER% == 6.0.6000  (
+GOTO :WINDOWSVISTA
+
+) ELSE IF %WINVER% == 6.0.6001  (
+GOTO :WINDOWSVISTA
+
+) ELSE IF %WINVER% == 6.0.6002  (
+GOTO :WINDOWSVISTA
+
+) ELSE IF %WINVER% == 6.1.7600 (
+GOTO :WINDOWS7
+
+) ELSE (
+ECHO VERSION NOT RETRIEVED - Uninstall Manually - Starting Add/Remove Programs...
+start appwiz.cpl
+PAUSE
+
+ECHO starting IE Properties...
+start inetcpl.cpl
+
+GO TO :END
+
+)
+
+
+
+
+
+:: WINDOWS VERSION CHECK
+
+IF %64BIT% == 1 (
+ECHO 64BIT
+)
+
+
+
+:WINDOWSXP
+
+ECHO WINDOWS XP
+ECHO Intiating Dial-a-Fix...
+start Dial-a-Fix\Dial-a-fix.exe
+PAUSE
+
+
+
+
+GOTO COMMONTOOLS
+
+:WINDOWSXP64
+
+ECHO WINDOWSXP 64
+PAUSE
+GOTO COMMONTOOLS
+
+:WINDOWSVISTA
+
+ECHO WINDOWS VISTA
+PAUSE
+
+PAUSE
+
+GOTO COMMONTOOLS
+
+:WINDOWS7
+
+ECHO WINDOWS 7
+PAUSE
+
+GOTO COMMONTOOLS
+
+
+
+
+
+
+
+
+
+:COMMONTOOLS
+
+ECHO.
+ECHO
+ECHO Enabling Firewall...
+netsh firewall set opmode enable
+
+netsh advfirewall set currentprofile state on
+PAUSE
+
+
+ECHO.
+ECHO Reset IE Settings...
+start inetcpl.cpl
+PAUSE
+
+
+ECHO.
+ECHO Uninstall CCleaner...
+
+cd %ccleanerpath%
+start uninst.exe /S
+PAUSE
+
+
+ECHO.
+ECHO Unistall Malwarebytes...
+
+cd %mbampath%
+start unins000.exe /SILENT
+PAUSE
+
+::radialpoint unistall
+::C:\Program Files\Radialpoint Malware Removal Tool/unins000.exe
+
+ECHO.
+ECHO Unistall SuperAntiSpyware... 
+
+cd %saspath%
+start Uninstall.exe
+
+
+
+
+
+
+
+:END
+
+
+:: SELF DELETING BATCH FILE
+:: del %0
+:: copy to c: make batch file seperate out of tools file then rmdir?
+
+:: Add automatic deletion of tools?
+
+
+
---
+++ Tools.cmd
@@ -0,0 +1,96 @@
+:: Off Process PCR Tools.
+
+CLS
+@echo off
+TITLE Starting up ToolBox 
+
+
+
+
+:: CHECK WINDOWS VERSION
+for /f "tokens=2 delims=[]" %%x in ('ver') do set WINVER=%%x 
+set WINVER=%WINVER:Version =% 
+
+ECHO %WINVER%
+
+
+
+
+
+:COMMONTOOLS
+
+ECHO.
+ECHO Initiating Windows Installer for Safe Mode...
+ECHO Enabling Windows Installer in Safe Mode (Minimal)
+%windir%\system32\REG.EXE ADD "HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Minimal\MSIServer" /VE /T REG_SZ /F /D "Service" > NUL
+IF %ERRORLEVEL% GTR 0 GOTO ERR
+IF %ERRORLEVEL% EQU 0 ECHO Windows Installer service is now available in Safe Mode (Minimal).
+ECHO .
+
+REM update registry to allow MSISERVER to run in Safe Mode w/Networking
+ECHO Enabling Windows Installer in Safe Mode (Networking)
+%windir%\system32\REG.EXE ADD "HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\MSIServer" /VE /T REG_SZ /F /D "Service" > NUL
+IF %ERRORLEVEL% GTR 0 GOTO ERR
+IF %ERRORLEVEL% EQU 0 ECHO Windows Installer service is now available in Safe Mode (Networking).
+ECHO .
+
+REM start the Windows Installer service
+ECHO Starting Windows Installer service
+%windir%\system32\NET.EXE start msiserver > NUL
+IF %ERRORLEVEL% GTR 0 GOTO ERR
+IF %ERRORLEVEL% EQU 0 ECHO The Windows Installer service been has started.
+ECHO .
+ECHO You may now add or remove programs from the computer.
+
+GOTO MSIDONE
+
+:ERR
+ECHO .
+ECHO An error has occurred trying to enabled MSI in Safe Mode.
+
+:MSIDONE
+
+
+ECHO.
+ECHO Initiating Add Remove Programs and MSCONFIG....
+start appwiz.cpl
+
+start MSCONFIG
+PAUSE
+
+
+ECHO.
+ECHO Initiating HiJackThis...
+start HiJackThis\HijackThis.exe
+PAUSE
+
+
+
+IF %WINVER% == 5.1.2600 (
+ECHO.
+ECHO Initiating SmitFraudFix...
+start SmitfraudFix.exe
+PAUSE
+
+) 
+
+ECHO.
+ECHO Initiating CCleaner ...
+start CCleaner\ccsetup301_slim.exe
+::/D=C:\dirhere
+::/S
+::/AUTO
+PAUSE
+
+
+ECHO.
+ECHO Initiating Malwarebytes...
+start Malwarebytes\mbam-setup.exe 
+::/silent
+PAUSE
+
+
+ECHO.
+ECHO Initiating Superantispyware...
+start SUPERAntiSpyware.exe
+
